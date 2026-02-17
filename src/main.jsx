2cad461804ae0e65e430685d0e07037499bbd7b3
smallproject <j.a.villas@live.com.ph>
2026-02-18 00:46:17 +0100

    added bootstrap css

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// bootstrap added via npm, so we can import the css and js directly here
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // needed for navbar toggle, modals, etc.
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
