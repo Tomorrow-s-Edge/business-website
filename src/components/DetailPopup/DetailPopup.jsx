a3fa9022edafeeaf4246f181840691cd9b8e4f29
smallproject <j.a.villas@live.com.ph>
2026-03-20 17:10:56 +0100

    temporarily removed

--- src/components/DetailPopup/DetailPopup.jsx
+++ src/components/DetailPopup/DetailPopup.jsx
@@ -1,18 +1,17 @@
-import "./PopupStyle.css"
-function DetailPage({item, onClose}) {
-    if (!item) return null
+import "./PopupStyle.css";
 
-    return(
+function DetailPopup({item, onClose }) {
+    if (!item) return null;
+
+    return (
         <div className="backdrop" onClick={onClose}>
             <div className="popup" onClick={(e) => e.stopPropagation()}>
-                <button className="close-btn" onClick={onClose}>x</button>
+                <button onClick={onClose}>Close</button>
                 <h2>{item.title}</h2>
                 <p>{item.description}</p>
-
             </div>
         </div>
     );
-
 }
 
-export default DetailPage;
+export default DetailPopup;
